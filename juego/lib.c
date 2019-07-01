8927dfbbe3ba119ba10c783eb48934627d0c1309
Martin Villagra <mvillagra0@gmail.com>
2019-07-01 12:56:26 -0300

    fix include paths of game

--- juego/lib.c
+++ juego/lib.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 
 #define DEBUG
 
